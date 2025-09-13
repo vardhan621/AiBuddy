47463196fb8e73f2b1c964ae575e01eb6fa66519
vardhan621 <hv4347172@gmail.com>
2025-09-13 16:39:58 +0530

    Add files via upload

---
+++ Chatbot2.o (2)/index.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import requests
+client = OpenAI(api_key="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")
+def chat_with_gpt(user_message):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",  # free-tier friendly
+        messages=[
+            {"role": "system", "content": "You are a helpful chatbot."},
+            {"role": "user", "content": user_message}
+        ]
+    )
+    return response.choices[0].message.content
+def image_gen(user_message):
+    result = client.images.generate(
+      model="gpt-image-1",
+      prompt=user_message,
+      size="1024x1024"  # Options: 256x256, 512x512, 
+      
+    )
+    image_url = result.data[0].url
+    print("Image URL:", image_url)
+    img_data=requests.get(image_url).content 
+    with open("robo.png","wb")as f:
+       f.write(img_data)
+    print("imagesaved")
+def link_gen(user_message):
+    response =  client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": user_message}]
+    )
+    print(response['choices'][0]['message']['content'])
